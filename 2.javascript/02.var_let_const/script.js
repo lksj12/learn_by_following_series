6b374f8c1ea515e965980d80e4d93360a8f89c70
user <user@MacPro.local>
2026-05-13 00:47:28 +0900

    studying var let const

---
+++ 2.javascript/02.var_let_const/script.js
@@ -0,0 +1,26 @@
+var greeting = "hello";
+console.log(greeting);
+
+var greeting = "hi";
+console.log(greeting);
+
+greeting = "how are you?";
+console.log(greeting);
+
+let greeting2 = "hee hee";
+console.log(greeting2);
+
+// let greeting2 = "hee hee hee";
+// console.log(greeting2);
+
+greeting2 = "hee hee hee hee";
+console.log(greeting2);
+
+
+const gre = "he";
+console.log(gre);
+
+// const gre = "he2"
+
+// gre = "he";
+// console.log(gre);
