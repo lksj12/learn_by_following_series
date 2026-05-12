927b02346a294af4a0a8ed8f53c34ac7b58c41a5
user <user@MacPro.local>
2026-05-13 00:48:18 +0900

    hoisting of javascript

---
+++ 2.javascript/04.hoisting/script.js
@@ -0,0 +1,14 @@
+// console.log(greet);
+// var greet = "hello";
+
+// console.log(greet1);
+// let greet = "hello";
+
+// console.log(greet2);
+// const greet2 = "hello";
+
+func();
+
+function func() {
+    console.log("hoisting");
+}
