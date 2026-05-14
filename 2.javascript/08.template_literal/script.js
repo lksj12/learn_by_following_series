a412278a12d37ae901d4b7e5ec50cf4103bdf255
user <user@MacPro.local>
2026-05-14 21:43:21 +0900

    studying template literal with using backtick

---
+++ 2.javascript/08.template_literal/script.js
@@ -0,0 +1,12 @@
+console.log("string text line 1\nstring text line 2");
+console.log("string text line 1\n"+ "string text line 2");
+
+console.log(`string text line 1
+string text line 2`);
+
+const a = 5;
+const b = 10;
+
+console.log("5+10 = " + (a+b) + "and\nnot" + (2*a + b) + ".");
+console.log(`5+10=${a+b} and
+not ${2*a + b}.`)
