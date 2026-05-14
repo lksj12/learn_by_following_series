306474fc7668343d7a83bb5b103a7a77db5dad54
user <user@MacPro.local>
2026-05-14 21:21:37 +0900

    studying calculation of js

---
+++ 2.javascript/07.js_calculation/script.js
@@ -0,0 +1,49 @@
+const num1 = 20;
+const num2 = 10;
+
+let val;
+
+val = num1 + num2;
+console.log(val);
+
+val = num1 * num2;
+console.log(val);
+
+val = num1 - num2;
+console.log(val);
+
+val = num1 / num2;
+console.log(val);
+
+val = num1 % num2;
+console.log(val);
+
+val = Math.E;
+console.log(val);
+
+val = Math.PI;
+console.log(val);
+
+val = Math.round(2.4);
+console.log(val);
+
+val = Math.round(2.7);
+console.log(val);
+
+val = Math.ceil(2.4);
+console.log(val);
+
+val = Math.floor(2.7);
+console.log(val);
+
+val = Math.abs(-2);
+console.log(val);
+
+val = Math.min(1,2,3,4,5);
+console.log(val);
+
+val = Math.max(1,2,3,4,5);
+console.log(val);
+
+// random number between 1 and 20
+val = Math.floor(Math.random() * 20 + 1);
