deb7f2904ccea5ccf869ac4fc6a4bfe6ef60a0d3
user <user@MacPro.local>
2026-05-16 01:23:14 +0900

    studying window object

---
+++ 2.javascript/10.window_object/script.js
@@ -0,0 +1,15 @@
+console.log(window.outerWidth);
+console.log(window.outerHeight);
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
+console.log(window.scrollX);
+console.log(window.scrollY);
+
+console.log(window.location);
+
+// window.location.href = "주소"
+
+console.log(window.history);
+console.log(window.navigator);
