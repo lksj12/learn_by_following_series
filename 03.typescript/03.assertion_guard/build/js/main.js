05462c3c0a01d511b3fc910497bf07d19e393325
lksj12 <lksj14@gmail.com>
2026-05-27 02:17:42 +0000

    change Node to bundler in tsconfig and compile ts files

--- 03.typescript/03.assertion_guard/build/js/main.js
+++ 03.typescript/03.assertion_guard/build/js/main.js
@@ -1,12 +1,31 @@
 "use strict";
+// let foo = {};
+// foo.bar = 123;
+// foo.bas = "hello";
+let foo = {};
+foo.bar = 123;
+foo.bas = "hello";
+//Error!!
 // const bodyElement = document.querySelector('body');
-// if (bodyElement) {
-//     bodyElement.innerText = "Hello";
-// }
+// bodyElement.innerText = "Hello";
+// Avoiding Error
+const bodyElement1 = document.querySelector('body');
+bodyElement1.innerText = "Hello";
+const bodyElement2 = document.querySelector('body');
+bodyElement2.innerText = "Hello";
+const bodyElement3 = document.querySelector('body');
+if (bodyElement3) {
+    bodyElement3.innerText = "Hello";
+}
+// Wrong assertion case
 // function func(arg: string | null) {
-//     if(arg) {
-//         return arg.toLowerCase();
+//         return (arg as string).toLowerCase();
 //     }
 // }
-// func('hello');
-// func(null);
+function func(arg) {
+    if (arg) {
+        return arg.toLowerCase();
+    }
+}
+func('hello');
+func(null);
