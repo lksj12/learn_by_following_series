60b45848b1393cd4fbd02ca16aa16adaa264d6c4
lksj12 <lksj14@gmail.com>
2026-05-30 11:09:07 +0000

    commenting result

--- 03.typescript/12.mapped-types/src/main.ts
+++ 03.typescript/12.mapped-types/src/main.ts
@@ -17,17 +17,17 @@ type Users = 'John' | 'Han' | 'Kim';
 
 type UserFirstNames = { [K in Users]: string };
 
-const userFirstNameInfo: UserFirstNames2 = {
+const userFirstNameInfo: UserFirstNames = {
     John: 'Doe',
     Han: "Ho",  
     Kim: "jun",
 }
 
-type UserFirstNames2 = {
-    John: string,
-    Han: string,
-    Kim: string,
-}
+// type UserFirstNames = {
+//     John: string,
+//     Han: string,
+//     Kim: string,
+// }
 
 type Users2 = 'John' | 'Han' | 'Kim';
 
